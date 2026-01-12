c34c6789694853bc83bb3741e23ce74f220b6a7a
Rishabh Baruah <rishabhbaruah2022@vitbhopal.ac.in>
2026-01-12 18:49:39 +0530

    Intermediate

--- 4.py
+++ 4.py
@@ -14,3 +14,20 @@ response = requests.get(url)
 data = response.json()
 
 print(data)
+
+
+# With function
+import requests
+def get_weather(lat, lan):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lan}&current=temperature_2m"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+paris_data= get_weather(48.85, 2.35)  # Paris coordinates
+london_data = get_weather(51.51, -0.13)  # London coordinates
+new_york_data = get_weather(40.71, -74.01)  # New York coordinates
+
+print(paris_data)
+print(london_data)
+print(new_york_data)
